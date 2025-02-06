1df24bcc0e60167bc3a99dbc75c6c7e61984f9f3
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-02-06 11:56:50 +0100

    Added Magento\Store\Model\StoreManagerInterface in the constructor and displayed the current website, group (a.k.a. store) and store (a.k.a. store view).

--- Console/Command/GetStorePhoneCommand.php
+++ Console/Command/GetStorePhoneCommand.php
@@ -7,6 +7,7 @@ namespace Denal05\Ad0e702ExerciseGetStorePhoneViaCli\Console\Command;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Store\Api\Data\StoreConfigInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -16,11 +17,13 @@ class GetStorePhoneCommand extends Command
 {
     private const PHONE = 'phone';
     protected ScopeConfigInterface $scopeConfig;
-
+    protected StoreManagerInterface $storeManager;
     public function __construct(
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManager
     ) {
         $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
         parent::__construct();
     }
 
@@ -57,7 +60,13 @@ class GetStorePhoneCommand extends Command
             $storePhone = $this->scopeConfig->getValue(
                 'general/store_information/phone'
             );
+            $currentWebsite = $this->storeManager->getWebsite()->getName();
+            $currentGroup =   $this->storeManager->getGroup()->getName();
+            $currentStore =   $this->storeManager->getStore()->getName();
             $output->writeln("<info>The store phone is: $storePhone</info>");
+            $output->writeln("<info>The current website is: $currentWebsite</info>");
+            $output->writeln("<info>The current group is: $currentGroup</info>");
+            $output->writeln("<info>The current store is: $currentStore</info>");
         } catch (LocalizedException $e) {
             $output->writeln(sprintf(
                 '<error>%s</error>',
